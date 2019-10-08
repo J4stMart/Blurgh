fb15a15e5cb61464c576be9c86039cbc43bfea81
Mart <m.vansaane2@students.uu.nl>
2019-10-08 16:02:05 +0200

    Update tutorial

--- BlurghUnity/Assets/Scripts/WarpTutorial.cs
+++ BlurghUnity/Assets/Scripts/WarpTutorial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarpTutorial : MonoBehaviour
 {
@@ -10,13 +11,20 @@ public class WarpTutorial : MonoBehaviour
     [SerializeField]
     private WarpMeshTutorial script;
 
+    [SerializeField]
+    private Text textElement;
+
     public Transform gravityPoint;
     public float gravityDistance;
     public float curvature;
     public float maxDeformation;
+    public float minHandDistance;
     public float maxHandDistance;
 
     private float deformation;
+    private bool activated;
+    private LineRenderer line;
+    private Color color;
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +33,41 @@ public class WarpTutorial : MonoBehaviour
         script.gravityDistance = gravityDistance;
         script.curvature = curvature;
         script.deformation = 0;
+
+        textElement.text = "Hold your hands close to eachother\nAnd press either of the index finger triggers\n\nTo active the gravity field";
+        line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var dist = Vector3.Distance(leftHand.position, rightHand.position);
-        var targetDeformation = Mathf.Clamp(dist, 0, maxHandDistance);
-        script.deformation = Mathf.Lerp(script.deformation, targetDeformation, dist / maxHandDistance);
+        var dist = Vector3.Distance(leftHand.position, rightHand.position) - minHandDistance;
 
+        if(activated)
+        {
+            var targetDeformation = Mathf.Clamp(dist, 0, maxHandDistance);
+            script.deformation = Mathf.Lerp(script.deformation, targetDeformation, dist / maxHandDistance);
+            color = Color.Lerp(Color.green, Color.red, dist / maxHandDistance);
+
+            line.SetPosition(0, leftHand.position);
+            line.SetPosition(1, rightHand.position);
+            line.SetColors(color, color);
+        }
+        else
+        {
+            if(dist <= minHandDistance && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                activated = true;
+                textElement.text = "Move your hands away from eachother\nAnd experience a gravity well";
+                line.enabled = true;
+            }
+        }
+        
+        if(!OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            activated = false;
+            textElement.text = "Hold your hands close to eachother\nAnd press either of the index finger triggers\n\nTo active the gravity field";
+            line.enabled = true;
+        }
     }
 }
