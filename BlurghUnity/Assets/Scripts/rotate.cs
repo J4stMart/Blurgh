699365c534f3ce5109ef28659c804c7732deb926
erik_wiersma <erikwiersma97@gmail.com>
2019-10-10 09:19:30 +0200

    rotatie scrip (nog niet af)

---
+++ BlurghUnity/Assets/Scripts/rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float RotationSpeed = 1.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "L") 
+        {
+            Quaternion currentRotation = other.transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 10);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * 4);
+        }
+        else if (other.gameObject.name == "R")
+        {
+            Quaternion currentRotation = other.transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, -10);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * 4);
+        }
+        else
+        {
+            Quaternion currentRotation = other.transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * 4);
+        }
+    }
+}
